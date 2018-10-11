e55719ba486d01c7482a970a8b3706af96903f66
yhoshua ochoa <benjamin.sanchez.zebadua.contractor@bbva.com>
2018-10-11 15:12:08 -0500

    maxchar algorithm

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/*
+function maxChar(str) {
+    const array = str.split('');
+    const mySet = Array.from(new Set(array));
+    let count = 0;
+    let char;
+  
+    const filtered = mySet.map((setiItem) => {
+     return array.filter((arrayElement) => arrayElement === setiItem);
+    });
+  
+    filtered.map((item) => {
+       if(item.length > count){
+         count = item.length;
+         char = item[0];
+       }
+    });
+  
+   return char
+}
+*/
+
+function maxChar(str) {
+    const charsMap = {};
+    let maxChar = '';
+    let maxNumber = 0;
+
+    for(let char of str){
+      charsMap[char] = charsMap[char] + 1 || 1;
+    }
+
+    for(let key in charsMap){
+        if(charsMap[key] > maxNumber){
+           maxNumber = charsMap[key];
+           maxChar = key;
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
